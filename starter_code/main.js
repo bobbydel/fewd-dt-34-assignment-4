d8a000a06b9b7be4b2fd265c1cb404b2840a3a54
Bobby <bobbydeltondo90@gmail.com>
2018-03-12 22:18:32 -0700

    Done

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,1 +1,31 @@
-//test 
+$(document).ready(documentready);
+
+function documentready() {
+
+    $(".learnmore").click(LearnMoreClick);
+}
+//learn more
+function LearnMoreClick() {
+    event.preventDefault();
+    $("#learnmoretext").slideDown();
+    $(".learnmore").hide();
+}
+//read more
+$(".readmore").click(ReadMoreClick);
+
+function ReadMoreClick() {
+    event.preventDefault();
+    $("#show-this-on-click").slideDown();
+    $(".readmore").hide();
+    $(".readless").show();
+}
+//read less
+$(".readless").click(ReadLessClick);
+
+function ReadLessClick() {
+    event.preventDefault();
+    $(".readless").hide();
+    $("#show-this-on-click").slideUp();
+    $(".readmore").show();
+
+}
